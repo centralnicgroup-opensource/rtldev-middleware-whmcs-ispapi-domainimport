5af39a9150ff40ab28beeada22cc9bf6f932ee8a
Kai Schwarz <kschwarz@hexonet.net>
2019-02-22 11:30:18 +0100

    refactor(translation): added german translation file for future use

---
+++ modules/addons/ispapidomainimport/lang/german.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * addon module language file.
+ * Language: German
+ */
+
+// results / errors
+$_ADDONLANG['ok'] = "OK";
+$_ADDONLANG['registrarerror'] = "Die Authentifizierung über das ISPAPI Registrar Modul ist fehlgeschlagen! Bitte prüfen Sie Ihre Logindaten und versuchen Sie es erneut.";
+$_ADDONLANG['actionerror'] = "Ungültige Modulaktion. Bitte gehen Sie zurück und versuchen Sie es erneut.";
+$_ADDONLANG['domaincreateerror'] = "Domain konnte nicht angelegt werden.";
+$_ADDONLANG['tldrenewalpriceerror'] = "Domainverlängerungspreis konnte nicht ermittelt werden.";
+$_ADDONLANG['registrantcreateerror'] = "Kunde konnte nicht angelegt werden.";
+$_ADDONLANG['registrantfetcherror'] = "Daten des Registranten konnten nicht geladen werden.";
+$_ADDONLANG['registrantmissingerror'] = "Der Domain ist kein Registrant zugeordnet.";
+$_ADDONLANG['alreadyexistingerror'] = "Domain existiert bereits.";
+$_ADDONLANG['domainnameinvaliderror'] = "Ungültiger Domainname.";
+$_ADDONLANG['nogatewayerror'] = "Kein Zahlungssystem konfiguriert.";
+$_ADDONLANG['domainlistfetcherror'] = "Domainliste konnte nicht geladen werden.";
+
+// headings
+$_ADDONLANG['h2.fetchdomainlist'] = "Domainliste laden";
+$_ADDONLANG['h2.importdomainlist'] = "Domainliste importieren";
+
+// labels
+$_ADDONLANG['label.domain'] = "Domain";
+$_ADDONLANG['label.domains'] = "Domains";
+$_ADDONLANG['label.gateway'] = "Zahlungssystem";
+$_ADDONLANG['label.currency'] = "Währung";
+
+// placeholders
+$_ADDONLANG['ph.domainfilter'] = "Domainnamefilter eingeben";
+
+// buttons
+$_ADDONLANG['bttn.pulldomainlist'] = "Laden";
+$_ADDONLANG['bttn.importdomainlist'] = "Importieren";
+$_ADDONLANG['bttn.back'] = "Zurück";
+
+// columns
+$_ADDONLANG['col.domain'] = "Domain";
+$_ADDONLANG['col.importresult'] = "Ergbnis des Imports";
